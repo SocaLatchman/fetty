0c5bce9fb9a85632669db9ead241d387f745bf5c
SocaLatchman <r.latchman3624@gmail.com>
2026-09-06 06:05:41 -0700

    Wrote a get_random_image function to return a random image to display on the homepage

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import os, random
 
 app = Flask(__name__)
 
+
+def get_random_image():
+    '''
+    get all the images from a directory, iterate over them and if its a
+    hidden file skip it. If its not, append it the file name to a list
+    and return a random item from the list.
+    '''
+    image_list = []
+    img_dir = os.getcwd() + "/static/images/giphy"
+    with os.scandir(img_dir) as images:
+        for image in images:
+            if image.name.startswith('.'):
+                continue
+            else:
+                image_list.append(image.name)
+    return random.choice(image_list)
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', random_image=get_random_image())
 
 
 if __name__ == '__main__':
